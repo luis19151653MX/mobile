60d3738cbad184e050b5011a6cb371dfc4b2d10b
luis19151653MX <19151653@aguascalientes.tecnm.mx>
2023-02-09 08:43:12 -0600

    En container

--- components/ImagenCarro.js
+++ components/ImagenCarro.js
@@ -1,19 +1,33 @@
-import { AspectRatio, Box,Image } from "native-base";
+import { AspectRatio, Box, Container, Image } from "native-base";
 import React from "react";
 
 
 export default function ImagenCarro() {
     return (
-        <AspectRatio ratio={{
-            base: 3 / 4,
-            md: 9 / 10
-        }} height={{
-            base: 200,
-            md: 400
-        }}>
-            <Image resizeMode="cover" source={{
-                uri: "https://images.vexels.com/media/users/3/224185/isolated/preview/357e905b6b7309dc770781c0edfef2e8-logotipo-geometrico-de-coche-electrico.png"
-            }} alt="Picture of an car" />
-        </AspectRatio>
+        <Container>
+
+            <AspectRatio ratio={{
+                base: 3 / 4,
+                md: 9 / 10
+            }} height={{
+                base: 200,
+                md: 400
+            }}>
+                <Image resizeMode="cover" source={{
+                    uri: "https://images.vexels.com/media/users/3/224185/isolated/preview/357e905b6b7309dc770781c0edfef2e8-logotipo-geometrico-de-coche-electrico.png"
+                }} alt="Picture of an car" />
+            </AspectRatio>
+            <AspectRatio ratio={{
+                base: 3 / 4,
+                md: 9 / 10
+            }} height={{
+                base: 200,
+                md: 400
+            }}>
+                <Image resizeMode="cover" source={{
+                    uri: "https://images.vexels.com/media/users/3/224185/isolated/preview/357e905b6b7309dc770781c0edfef2e8-logotipo-geometrico-de-coche-electrico.png"
+                }} alt="Picture of an car" />
+            </AspectRatio>
+        </Container>
     );
 }
