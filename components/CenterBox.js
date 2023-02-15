6739badc39b84e7cb394583f35a4bbad0073d6fe
luis19151653MX <19151653@aguascalientes.tecnm.mx>
2023-02-14 23:44:32 -0600

    Center con botones

--- components/CenterBox.js
+++ components/CenterBox.js
@@ -15,7 +15,7 @@ export default function CenterBox() {
                 <Button colorScheme="danger" onPress={()=>console.log("Presionado")}>
                     Boton
                 </Button>
-                <Button colorScheme="tema.5" onPress={()=>console.log("Presionado")}>
+                <Button colorScheme="tema.6" onPress={()=>console.log("Presionado")}>
                     Boton
                 </Button>
             </VStack>
