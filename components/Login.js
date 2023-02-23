2e99296395cd818de39820c4dc6d6109be45a3d0
luis19151653MX <19151653@aguascalientes.tecnm.mx>
2023-02-22 20:32:41 -0600

    funciona Login 50%

--- components/Login.js
+++ components/Login.js
@@ -4,36 +4,32 @@ import { VStack, FormControl, Input, Button, useToast, Box } from "native-base";
 export default function Login() {
     const toast = useToast();
     //puede ser asi o con el import de sseState
-    const [formData, setData] = useState({nickname:'',password:''});
-    const [errors, setErrors] = useState({});
-
-    const onChange = (e) => {
-        e.persist();
-        setData({ ...formData, [e.target.name]: e.target.value });
-      }
+    const [formData, setFormData] = useState({ nickname: undefined, password: '' })
+    const [errorsData, setErrorsData] = useState({})
 
     //===
     const Validate = () => {
-        //los ... son para ir concatenano los erroes, esto se hace en los objetos {}
+        //los ... son para ir concatenano los errores, esto se hace en los objetos {}
         if (formData.nickname === undefined) {
-            setErrors({ ...errors, nickname: "Nickname is required" });
+            setErrorsData({ ...errorsData, error: "Nickname is required" });
+            return false;
+        } else if (formData.nickname.length < 4) {
+            //setErrorsData({});
+            setErrorsData({ ...errorsData, error: "Nickname min length 4" });
             return false;
         }
-
-        if (formData.nickname.length < 4) {
-            setErrors({ ...errors, nickname: "Nickname min length 4" });
-            return false;
-        }
+        //setErrorsData({});
         return true;
     }
 
-
     const submit = () => {
+        setErrorsData({});
         Validate() ?
             (
+                setErrorsData({}),
                 console.log("ok"),
                 toast.show({
-                    description: { formData }
+                    description: "Bienvenido " + formData.nickname
                 })
             )
             :
@@ -41,8 +37,8 @@ export default function Login() {
                 console.log("bad"),
                 toast.show({
                     render: () => {
-                        return <Box bg="emerald.500" px="2" py="1" rounded="sm" mb={5}>
-                            Hello! Have a nice day {errors}
+                        return <Box bg="secondary.500" px="2" py="1" rounded="sm" mb={5}>
+                            {errorsData.error}
                         </Box>
                     }
                 })
@@ -52,20 +48,22 @@ export default function Login() {
     //isInvalid
     return (
         <VStack>
-            <FormControl isRequired isInvalid={'nickname' in errors} minLength={4} maxLength={8}>
-                <FormControl.Label>Favorite framework</FormControl.Label>
-                <Input p={2} placeholder="Is it react?" style={{ color: "#000000" }} onChangeText={value=>setData({...formData,nickname: value})} />
+            <FormControl isRequired isInvalid={'error' in errorsData}>
+                <FormControl.Label>Nickname</FormControl.Label>
+                <Input p={2} w="80%" placeholder="Is it react?" style={{ color: "#000000" }} onChangeText={value => setFormData({ ...formData, nickname: value })} />
 
-                {'nickname' in errors ?
-                    (
-                        <FormControl.ErrorMessage>{errors}</FormControl.ErrorMessage>
-                    )
-                    :
-                    (
-                        <FormControl.HelperText>
-                            We'll keep this between us.
-                        </FormControl.HelperText>
-                    )}
+                {
+                    'error' in errorsData ?
+                        (
+                            <FormControl.ErrorMessage>Advertencia: {errorsData.error}</FormControl.ErrorMessage>
+                        )
+                        :
+                        (
+                            <FormControl.HelperText>
+                                Ingresa tus datos
+                            </FormControl.HelperText>
+                        )
+                }
 
                 <Button
                     bg="tema.5"
