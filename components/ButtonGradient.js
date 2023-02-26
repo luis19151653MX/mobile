38a4f6d1513802ca43456d43afcf237e8cf3dcc8
luis19151653MX <19151653@aguascalientes.tecnm.mx>
2023-02-25 19:55:02 -0600

    onPress funciona

--- components/ButtonGradient.js
+++ components/ButtonGradient.js
@@ -1,26 +1,33 @@
-import { Box,Button } from "native-base";
+import { Box, Button, Pressable, Text } from "native-base";
 import React, { useState } from "react";
 
-export default function Login(ingresar) {
+export default function Login(props) {
     const LinearGradient = require('expo-linear-gradient').LinearGradient;
     return (
-        <Box rounded="md" bg={{
-            linearGradient: {
-                colors: ['lightBlue.800', 'lightBlue.300'],
-                start: [0, 0],
-                end: [2, 0]
-            }
-        }}
-         >
-            <Button variant="unstyled" _text={{
-                fontSize: 'md',
-                fontWeight: 'medium',
-                color: 'warmGray.50',
-                textAlign: 'center'
-            }} 
-            onPress={()=>ingresar}>
-                Ingresar
-            </Button>
-        </Box>
+        <Pressable
+            onPress={() => {
+                console.log('hell'),
+                    props.ingresar()
+            }}
+
+        >
+            <Box p="2" rounded="md" bg={{
+                linearGradient: {
+                    colors: ['lightBlue.800', 'lightBlue.300'],
+                    start: [0, 0],
+                    end: [2, 0]
+                }
+            }}
+                _text={{
+                    fontSize: 'lg',
+                    fontWeight: 'medium',
+                    color: 'warmGray.50',
+                    textAlign: 'center'
+                }}
+            >Ingresar
+            </Box>
+        </Pressable>
+
+
     );
 }
